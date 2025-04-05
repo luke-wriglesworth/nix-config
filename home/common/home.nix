133cdb6f57a5c0066bff5eea72fa90d08630b76e
Luke Wriglesworth <luke.wriglesworth@mail.utoronto.ca>
2025-04-04 21:01:36 -0400

    ...

--- home/common/home.nix
+++ home/common/home.nix
@@ -72,20 +72,24 @@
       enable = true;
       enableZshIntegration = true;
       extraConfig = ''
-                     local config = wezterm.config_builder()
-                     config.font_size = 12.0
-                     config.font = wezterm.font "JetbrainsMono Nerd Font"
-                     config.enable_wayland = true
-                     config.audible_bell = "Disabled"
-        		 config.window_close_confirmation = 'NeverPrompt'
-        				 config.enable_tab_bar = false
-                     config.window_padding = {
-            	left = '1cell',
-            	right = '1cell',
-            	top =0,
-            	bottom = 0,
-            }
+        local config = wezterm.config_builder()
+                   config.font = wezterm.font "JetbrainsMono Nerd Font"
+                   config.enable_wayland = true
+                   config.audible_bell = "Disabled"
+        config.window_close_confirmation = 'NeverPrompt'
+             			config.enable_tab_bar = false
+                   config.window_padding = {
+        	left = '1cell',
+        	right = '1cell',
+        	top =0,
+        	bottom = 0,
+        }
         ${lib.optionalString pkgs.stdenv.isLinux "config.window_decorations = 'NONE'"}
+        ${
+          if pkgs.stdenv.isDarwin
+          then "config.font_size = 16.0"
+          else "config.font_size = 12.0"
+        }
         return config
       '';
     };
