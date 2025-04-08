8740d369ff59a75a3fed57be3c5b5a9eb0fc2fc6
Luke Wriglesworth <luke.wriglesworth@mail.utoronto.ca>
2025-04-08 01:26:42 -0400

    ...
    ...

--- config/darwin/darwin.nix
+++ config/darwin/darwin.nix
@@ -46,11 +46,71 @@
       "2606:4700:4700::1001"
     ];
   };
+  system.activationScripts = {
+    extraActivation = {
+      text = ''
+        #!/bin/bash
+        sudo yabai --load-sa
+        yabai -m space --destroy
+        yabai -m space --destroy
+        yabai -m space --destroy
+        yabai -m space --destroy
+        yabai -m space --create
+        yabai -m space --create
+        yabai -m space --create
+        yabai -m space --create
+        echo "displays created..."
+      '';
+    };
+  };
 
   services = {
+    yabai = {
+      enable = true;
+      enableScriptingAddition = true;
+      config = {
+        layout = "bsp";
+        focus_follows_mouse = "autoraise";
+        mouse_follows_focus = "off";
+        window_placement = "second_child";
+        window_opacity = "off";
+        top_padding = 10;
+        bottom_padding = 10;
+        left_padding = 10;
+        right_padding = 10;
+        window_gap = 10;
+      };
+      extraConfig = ''
+        sudo yabai --load-sa
+      '';
+    };
+    skhd = {
+      enable = true;
+      skhdConfig = ''
+        alt - h : yabai -m window --focus west
+        alt - j : yabai -m window --focus south
+        alt - k : yabai -m window --focus north
+        alt - l : yabai -m window --focus east
+        shift + alt - h : yabai -m window --warp west
+        shift + alt - j : yabai -m window --warp south
+        shift + alt - k : yabai -m window --warp north
+        shift + alt - l : yabai -m window --warp east
+        alt - 1 : yabai -m space --focus 1
+        alt - 2 : yabai -m space --focus 2
+        alt - 3 : yabai -m space --focus 3
+        alt - 4 : yabai -m space --focus 4
+        alt - 5 : yabai -m space --focus 5
+        shift + alt - 1 : yabai -m window --space 1
+        shift + alt - 2 : yabai -m window --space 2
+        shift + alt - 3 : yabai -m window --space 3
+        shift + alt - 4 : yabai -m window --space 4
+        shift + alt - 5 : yabai -m window --space 5
+      '';
+    };
+
     jankyborders.enable = true;
     sketchybar = {
-      enable = true;
+      enable = false;
       config = ''
               sketchybar --bar position=top height=40 blur_radius=30 color=0x40000000
               default=(
@@ -87,53 +147,6 @@
     };
     tailscale = {
       enable = true;
-    };
-    aerospace = {
-      enable = true;
-      settings = {
-        #exec-on-workspace-change = [
-        #  "/bin/bash"
-        #  "-c"
-        # "sketchybar --trigger aerospace_workspace_change FOCUSED=$AEROSPACE_FOCUSED_WORKSPACE"
-        #];
-        gaps = {
-          inner.horizontal = 10;
-          inner.vertical = 10;
-          outer.left = 8;
-          outer.bottom = 8;
-          outer.top = 4;
-          outer.right = 8;
-        };
-        mode.main.binding = {
-          alt-h = "focus left";
-          alt-j = "focus down";
-          alt-k = "focus up";
-          alt-l = "focus right";
-          alt-1 = "workspace 1";
-          alt-2 = "workspace 2";
-          alt-3 = "workspace 3";
-          alt-4 = "workspace 4";
-          alt-5 = "workspace 5";
-          alt-6 = "workspace 6";
-          alt-7 = "workspace 7";
-          alt-8 = "workspace 8";
-          alt-9 = "workspace 9";
-          alt-0 = "workspace 10";
-          alt-shift-1 = "move-node-to-workspace 1";
-          alt-shift-2 = "move-node-to-workspace 2";
-          alt-shift-3 = "move-node-to-workspace 3";
-          alt-shift-4 = "move-node-to-workspace 4";
-          alt-shift-5 = "move-node-to-workspace 5";
-          alt-shift-6 = "move-node-to-workspace 6";
-          alt-shift-7 = "move-node-to-workspace 7";
-          alt-shift-8 = "move-node-to-workspace 8";
-          alt-shift-9 = "move-node-to-workspace 9";
-          alt-slash = "layout tiles horizontal vertical";
-          alt-comma = "layout accordion horizontal vertical";
-          alt-q = "exec-and-forget open -n ~/.nix-profile/Applications/WezTerm.app";
-          alt-w = "close --quit-if-last-window";
-        };
-      };
     };
   };
 
