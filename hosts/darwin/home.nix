d06f923b9cd27295e3a10a62d10701933c986534
Luke Wriglesworth <luke.wriglesworth@mail.utoronto.ca>
2025-05-02 14:34:40 -0400

    ...

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -1,7 +1,2 @@
 {inputs, ...}: {
-  imports = [inputs.lan-mouse.homeManagerModules.default];
-  programs.lan-mouse = {
-    enable = false;
-    settings.authorized_fingerprints = {"55:f5:98:93:c6:c8:ec:b7:cd:36:30:e3:ab:5b:fb:ae:18:fc:dd:d2:3e:8f:19:5a:b5:3b:ef:92:64:89:82:05" = "nixos";};
-  };
 }
